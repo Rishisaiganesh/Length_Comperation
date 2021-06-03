31a498aa3ee814bd1634e1a0cbdd4038fbfa0436
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-03 11:20:29 +0530

    [Rishi]Opps:Lengthofline

---
+++ Length_of_line/Length_of_line/Lengthofline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Length_of_line
+{
+    class Lengthofline
+    {
+        public void Calculate(int x1, int y1, int x2, int y2)
+        {
+            float length = 0;
+            length = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("length iss=" + length);
+        }
+    }
+}
---
+++ Length_of_line/Length_of_line/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Length_of_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Length of line");
+            Lengthofline Length = new Lengthofline();
+            Length.Calculate(1, 1, 1, 1);
+            Console.ReadKey();
+        }
+    }
+}
